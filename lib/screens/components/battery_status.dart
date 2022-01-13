fc4976f7a64f224a7e12d35cb29a89dc611e7c49
Vika Belskaya <vika.bel773@gmail.com>
2022-01-13 17:46:46 +0300

    battery status

---
+++ lib/screens/components/battery_status.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import '../../contanins.dart';
+
+class BatteryStatus extends StatelessWidget {
+  const BatteryStatus({
+    Key? key,
+    required this.constrains,
+  }) : super(key: key);
+
+  final BoxConstraints constrains;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          '220 mi',
+          style: Theme.of(context)
+              .textTheme
+              .headline3!
+              .copyWith(color: Colors.white),
+        ),
+        Text(
+          '62%',
+          style: TextStyle(
+            fontSize: 22,
+          ),
+        ),
+        Spacer(),
+        Text(
+          'Charging'.toUpperCase(),
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+        Text(
+          '16 min remaining',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        SizedBox(
+          height: constrains.maxHeight * 0.14,
+        ),
+        DefaultTextStyle(
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('22 mi/hr'),
+              Text('232 v'),
+            ],
+          ),
+        ),
+        const SizedBox(height: defaultPadding),
+      ],
+    );
+  }
+}
